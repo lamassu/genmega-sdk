7a36cc70d8b9001e2237cf9be77184831b3af8d1
José Oliveira <josepfo.w@gmail.com>
2022-12-26 12:25:24 +0000

    fix: module entrypoint, napi methods missing args

--- index.js
+++ index.js
@@ -2,3 +2,9 @@ const genmega = require('./build/Release/genmega.node')
 
 exports.BarcodeScan = genmega.BarcodeScan
 exports.BarcodeCancelScan = genmega.BarcodeCancelScan
+exports.BillValidatorStatus = genmega.BillValidatorStatus
+exports.BillValidatorEnable = genmega.BillValidatorEnable
+exports.BillValidatorDisable = genmega.BillValidatorDisable
+exports.BillValidatorStack = genmega.BillValidatorStack
+exports.BillValidatorReject = genmega.BillValidatorReject
+exports.BillValidatorInit = genmega.BillValidatorInit
