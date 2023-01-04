71e41c49fcc7a4190bb512db635132663008c676
josepfo <josepfo.w@gmail.com>
2023-01-04 17:14:15 +0000

    feat: add thread yield and loop timeout

--- src/devices/barcode-scanner.cpp
+++ src/devices/barcode-scanner.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <time.h>
 
 #include "genmegadevice/HmDeviceIF.h"
 #include "barcode-scanner.hpp"
@@ -30,6 +31,7 @@ std::string BCS_Scan(char* serialPortName, int mobilePhoneMode, int presentation
 
     unsigned char errmsg[6] = {0};
     int iRet = 0;
+    time_t StartTime, CurTime;
 
     BCS_CallBackRegister(ScannedBarcodeData);
 
@@ -50,11 +52,20 @@ std::string BCS_Scan(char* serialPortName, int mobilePhoneMode, int presentation
     iRet = BCS_AcceptScanCode(presentationMode);
 
     if (iRet == HM_DEV_OK) {
+        time(&StartTime);
         while (1) {
+            std::this_thread::yield(); // give priority to other process/threads
             if (scannedDataResult.canceled) {
                 iRet = BCS_CancelScanCode();
                 BCS_Close();
                 // should this return a custom error code informing that the scan was canceled intensionally?
+                return std::to_string(iRet);
+            }
+            time(&CurTime);
+            if((StartTime+30) < CurTime){
+                printf("30sec Timeout and Cancel\n");
+                iRet = BCS_CancelScanCode();
+                BCS_Close();
                 return std::to_string(iRet);
             }
             if (scannedDataResult.valid && scannedDataResult.scannedData->wSize > 0) {
