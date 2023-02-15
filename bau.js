b246aa1f24de27b6273b6b408f7a2ac927fd37d6
José Oliveira <josepfo.w@gmail.com>
2023-02-15 18:48:48 +0000

    fix: move bau enable to l-m

--- bau.js
+++ bau.js
@@ -1,11 +1,6 @@
 const _ = require('lodash/fp')
 const genmega = require('./build/Release/genmega.node')
 
-const SENDONLY = 1
-const RECVONLY = 2
-
-let acceptingBill = false
-
 exports.BAUOpen = function BAUOpen(serialPortName) {
     const { iRet, data } = genmega.BAUOpenV2(serialPortName);
     console.log('BAU Firmware Version: ', data);
@@ -34,8 +29,6 @@ exports.BAUStatus = function BAUStatus() {
     const { iRet, data } = genmega.BAUStatusV2();
     if(iRet < 0) console.error(`BAU STATUS: ${iRet}`);
     const values = data.split("")
-    //Alternatively, we can just return the current state 
-    //but for testing purposes, we want to check if only one state is active
     result.bLineStatus = values[0];
     result.bIdling = values[1];
     result.bAccepting = values[2];
@@ -61,43 +54,15 @@ exports.BAUSetEnableDenom = function BAUSetEnableDenom(denominationData) {
 
 
 exports.BAUCancel = function BAUCancel() {
-    acceptingBill = false
     const { iRet, data } = genmega.BAUCancelV2();
     if(iRet < 0) console.error(`BAU CANCEL BILL: ${iRet}`); 
     return { iRet, data };
 }
 
-
-// Returns the accepted denomination index or the iRet if an error occurred
-exports.BAUEnable = function BAUEnable() {
-    let interval;
-    let result = { iRet: 0, data: "" };
-
-    return new Promise((resolve, reject) => {
-        const { iRet, data } = genmega.BAUAcceptBillV2(SENDONLY);
-        if(iRet < 0) return resolve({ iRet });
-        acceptingBill = true;
-        if(data != '0') return resolve({ data });
-        interval = setInterval(() => {
-            if(!acceptingBill) {
-                clearInterval(interval);
-                return resolve({ iRet: -100 }) // Bill acceptance was canceled externally
-            }
-            const { iRet, data } = genmega.BAUAcceptBillV2(RECVONLY);
-            if(iRet < 0) {
-                clearInterval(interval);
-                return resolve({ iRet });
-            }
-            if(data != '0' && iRet != 3) {
-              result.data = data;
-              result.iRet = iRet;
-              clearInterval(interval);
-              if (result.data && (result.iRet == 0)) {
-                  return resolve({ data: result.data });
-              } else reject(new Error(`BAU ENABLE: ${result.iRet}`)); 
-            } 
-          }, 200);
-      })
+exports.BAUAcceptBill = function BAUAcceptBill(mode) {
+    const { iRet, data } = genmega.BAUAcceptBillV2(mode);
+    if(iRet < 0) console.error(`BAU ACCEPT BILL: ${iRet}`);
+    return { iRet, data }
 }
 
 exports.BAUReject = function BAUReject() {
