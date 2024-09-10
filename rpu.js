65c61177e6b8a09032acb8e5992c65652ae7aca6
siiky <github-siiky@net-c.cat>
2024-09-10 11:04:15 +0100

    feat: code and message return for `RPU`

--- rpu.js
+++ rpu.js
@@ -1,4 +1,6 @@
 const genmega = require('./build/Release/genmega.node')
+const return_codes = require('./return_codes')
+const return_messages = require('./return_messages')
 
 exports.getLastError = () => {
     const { data } = genmega._RPUGetLastError();
@@ -6,17 +8,21 @@ exports.getLastError = () => {
 }
 
 exports.open = serialPortName => {
-    const { return_int, data } = genmega._RPUOpen(serialPortName)
+    const { data, return_int } = genmega._RPUOpen(serialPortName)
+    const return_code = return_codes[return_int]
+    const return_message = return_messages[return_int]
     console.log('RPU Firmware Version: ', data)
-    if (return_int < 0) console.error('RPU OPEN: ', return_int)
-    return { return_int }
+    if (return_int < 0) console.error(`RPU OPEN: ${return_code} (${return_int}): ${return_message}`)
+    return { return_int, return_code, return_message }
 }
 
 exports.close = () => genmega._RPUClose()
 
 exports.status = () => {
-    const { return_int, data } = genmega._RPUStatus()
-    if (return_int < 0) console.error(`RPU STATUS: ${return_int}`)
+    const { data, return_int } = genmega._RPUStatus()
+    const return_code = return_codes[return_int]
+    const return_message = return_messages[return_int]
+    if (return_int < 0) console.error(`RPU STATUS: ${return_code} (${return_int}): ${return_message}`)
     if (data === "") console.log(`RPU STATUS EMPTY!`)
     const [
       LineStatus,
@@ -29,17 +35,21 @@ exports.status = () => {
       PaperNormal,
     ] = data
     const result = { LineStatus, PaperLoad, PaperTphLoad, PaperNearEnd, TphLever, PaperJam, CutterHome, PaperNormal }
-    return { return_int, result };
+    return { return_int, return_code, return_message, result }
 }
 
 exports.cutPaper = () => {
-    const { return_int } = genmega._RPUCutPaper()
-    if (return_int < 0) console.error('RPU CUT PAPER: ', return_int)
-    return { return_int }
+    const { data, return_int } = genmega._RPUCutPaper()
+    const return_code = return_codes[return_int]
+    const return_message = return_messages[return_int]
+    if (return_int < 0) console.error(`RPU CUT PAPER: ${return_code} (${return_int}): ${return_message}`)
+    return { return_int, return_code, return_message }
 }
 
 exports.printText = text => {
-    const { return_int } = genmega._RPUPrintText(text)
-    if (return_int < 0) console.error('RPU PRINT TEXT: ', return_int)
-    return { return_int }
+    const { data, return_int } = genmega._RPUPrintText(text)
+    const return_code = return_codes[return_int]
+    const return_message = return_messages[return_int]
+    if (return_int < 0) console.error(`RPU PRINT TEXT: ${return_code} (${return_int}): ${return_message}`)
+    return { return_int, return_code, return_message }
 }
