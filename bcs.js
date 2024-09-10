aae13500a8b75694e53bd7668e424ecf8bb5feba
siiky <github-siiky@net-c.cat>
2024-09-10 10:34:59 +0100

    feat: code and message return for `BCS`

--- bcs.js
+++ bcs.js
@@ -1,20 +1,28 @@
 const genmega = require('./build/Release/genmega.node')
+const return_codes = require('./return_codes')
+const return_messages = require('./return_messages')
 
 exports.scan = (serialPortName, mobilePhoneMode) => {
   return new Promise(resolve => {
     /* There's a timeout at ~30s, but we never learn of it... */
     const timeout = setTimeout(() => {
       exports.cancelScan()
-      return resolve({ iRet: -9, code: null })
+      const decoded = null
+      const return_int = -9
+      const return_code = return_codes[return_int]
+      const return_message = return_messages[return_int]
+      return resolve({ decoded, return_int, return_code, return_message })
     }, 31000)
 
     genmega._BCSScan(
       serialPortName,
       mobilePhoneMode,
-      (iRet, code) => {
+      (return_int, decoded) => {
         clearTimeout(timeout)
-        console.log("BCSScan resolving with iRet", iRet, "and code", code)
-        return resolve({ iRet, code })
+        console.log("BCSScan resolving with return_int", return_int, "and decoded", decoded)
+        const return_code = return_codes[return_int]
+        const return_message = return_messages[return_int]
+        return resolve({ decoded, return_int, return_code, return_message })
       }
     )
   })
